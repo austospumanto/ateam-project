b85a65bcbe369ee8a6be96e8ea55be6358a4f6ef
Vignesh Venkataraman <sendittovignesh@gmail.com>
2017-05-16 16:01:15 -0700

    Scrips for fetching and processing data into sqlite

---
+++ data/fetch_data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script will fetch the TIDIGITs dataset from Stanford AFS, with proper authentication
+# IMPORTANT: do NOT check the dataset into version control, do NOT share the dataset with anyone,
+# and do NOT use the dataset for anything other than this project. Delete the data once you have
+# finished using it.
+
+echo "Type in your SUNet ID (e.g. 'viggy'), followed by [Enter]:"
+
+read sunetid
+
+scp -r $sunetid@corn.stanford.edu:/afs/ir.stanford.edu/data/linguistic-data/ldc/LDC93S10_TIDIGITS .
