6841d3fee8fa3d9095c848676e3260c40655ad6d
Vignesh Venkataraman <sendittovignesh@gmail.com>
2017-05-17 23:00:38 -0700

    Add data dumping script

--- data/tidigits.py
+++ data/tidigits.py
@@ -1,5 +1,11 @@
+import pickle
 import random
 import sqlite3
+
+import python_speech_features as psf
+import scikits.audiolab
+
+from speech.model_utils import get_tidigits_to_index_mapping
 
 DB_NAME = 'data/tidigits.db'
 TIDIGITS_PATH = 'data/LDC93S10_TIDIGITS/%s/tidigits'
@@ -7,7 +13,6 @@ CDS = ['CD4_1_1', 'CD4_2_1', 'CD4_3_1']
 
 conn = sqlite3.connect(DB_NAME)
 conn.row_factory = sqlite3.Row
-
 
 def get_audio_file_path(desired_digits, desired_length=2):
     # Must have desired length, or else we prepend zeros
@@ -22,3 +27,43 @@ def get_audio_file_path(desired_digits, desired_length=2):
     choice = random.choice(rows)
     print choice
     return choice['path']
+
+def dump_data():
+    train_examples = []
+    train_sequences = []
+    train_seqlens = []
+    test_examples = []
+    test_sequences = []
+    test_seqlens = []
+    index_mapping = get_tidigits_to_index_mapping()
+    c = conn.cursor()
+    c.execute('select path, digits from tidigits where digits like ? or digits like ?;', ('z%', '1%'))
+    rows = c.fetchall()
+    random.shuffle(rows)
+    print len(rows)
+    cutoff = int(len(rows) * 0.8)
+    for row in rows[:cutoff]:
+        example, sequence, seq_len = get_mfcc_data(row['path'], row['digits'])
+        train_examples.append(example)
+        train_sequences.append(sequence)
+        train_seqlens.append(seq_len)
+    for row in rows[cutoff:]:
+        example, sequence, seq_len = get_mfcc_data(row['path'], row['digits'])
+        test_examples.append(example)
+        test_sequences.append(sequence)
+        test_seqlens.append(seq_len)
+    train_dataset = (train_examples, train_sequences, train_seqlens)
+    test_dataset = (test_examples, test_sequences, test_seqlens)
+    pickle.dump(train_dataset, 'data/train.dat')
+    pickle.dump(test, 'data/test.dat')
+
+def get_mfcc_data(path, digits):
+    f = scikits.audiolab.Sndfile(path, 'r')
+    signal = f.read_frames(f.nframes)
+    example = psf.mfcc(signal)
+    sequence = [index_mapping[ch] for ch in digits]
+    seq_len = example.shape[0]
+    return example, sequence, seq_len
+
+if __name__ == "__main__":
+    dump_data()
