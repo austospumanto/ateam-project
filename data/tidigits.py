a9995b4eb532d559ba89d182d412db1f196e56b7
Vignesh Venkataraman <sendittovignesh@gmail.com>
2017-06-04 17:37:40 -0700

    Add single digits to dataset

--- data/tidigits.py
+++ data/tidigits.py
@@ -134,8 +134,12 @@ def get_split_dataset():
     test_rows = fetchall_for_query((
         "select id, digits from tidigits where usage = 'test' and \
          (digits like ? or digits = ? or digits = ? or digits = ? \
-         or digits = ? or digits = ? or digits = ?);",
-        ('z%', '1z', '11', '12', '13', '14', '15')
+         (digits like ? or digits = ? or digits = ? or digits = ? \
+         (digits like ? or digits = ? or digits = ? or digits = ? \
+         (digits like ? or digits = ? or digits = ? or digits = ? \
+         or digits = ?);",
+        ('z', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+         'z%', '1z', '11', '12', '13', '14', '15')
     ))
     test_ids = [int(row['id']) for row in test_rows]
 
@@ -143,8 +147,12 @@ def get_split_dataset():
     train_rows = fetchall_for_query((
         "select id, digits from tidigits where usage = 'train' and \
          (digits like ? or digits = ? or digits = ? or digits = ? \
-         or digits = ? or digits = ? or digits = ?);",
-        ('z%', '1z', '11', '12', '13', '14', '15')
+         (digits like ? or digits = ? or digits = ? or digits = ? \
+         (digits like ? or digits = ? or digits = ? or digits = ? \
+         (digits like ? or digits = ? or digits = ? or digits = ? \
+         or digits = ?);",
+        ('z', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+         'z%', '1z', '11', '12', '13', '14', '15')
     ))
 
     # Part 2b - split non-test data into train and val splits, based on speaker_type
