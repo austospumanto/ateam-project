e40296e74d182027d2bfcd75be67bf79d431adc8
Vignesh Venkataraman <sendittovignesh@gmail.com>
2017-06-04 17:39:01 -0700

    Syntax :(

--- data/tidigits.py
+++ data/tidigits.py
@@ -138,7 +138,7 @@ def get_split_dataset():
          (digits like ? or digits = ? or digits = ? or digits = ? \
          (digits like ? or digits = ? or digits = ? or digits = ? \
          or digits = ?);",
-        ('z', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        ('z', '1', '2', '3', '4', '5', '6', '7', '8', '9', \
          'z%', '1z', '11', '12', '13', '14', '15')
     ))
     test_ids = [int(row['id']) for row in test_rows]
@@ -151,7 +151,7 @@ def get_split_dataset():
          (digits like ? or digits = ? or digits = ? or digits = ? \
          (digits like ? or digits = ? or digits = ? or digits = ? \
          or digits = ?);",
-        ('z', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        ('z', '1', '2', '3', '4', '5', '6', '7', '8', '9', \
          'z%', '1z', '11', '12', '13', '14', '15')
     ))
 
